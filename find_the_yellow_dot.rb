62df0dde8f694a4c5de605c31c4ef9f5da90400b
Balazs Varga <varbalazs@gmail.com>
2016-05-06 10:31:12 +0200

    3

--- find_the_yellow_dot.rb
+++ find_the_yellow_dot.rb
@@ -2,33 +2,33 @@
 
 require 'ev3dev'
 
-def red?
-  col.value0.to_i == 5
-end
-
-def yellow?
-  col.value0.to_i == 4
-end
-
 col   = Ev3dev::Sensor.new 3
 col.mode 'COL-COLOR'
 
 lm_b = Ev3dev::Motor.new 'B'
 lm_c = Ev3dev::Motor.new 'C'
 
-lm_b.duty_cycle_sp 30
-lm_c.duty_cycle_sp 30
+lm_b.duty_cycle_sp 70
+lm_c.duty_cycle_sp 70
 
 state = :finding_red
 
-while state != :found_red
-  if red?
-    state = :found_red
-  else
-    lm_b.command 'run-forever'
-    lm_c.command 'run-forever'
-  end
+def red?(col)
+  col.value0.to_i == 5
 end
+
+def yellow?(col)
+  col.value0.to_i == 4
+end
+
+# while state != :found_red
+#   if red?(col)
+#     state = :found_red
+#   else
+#     lm_b.command 'run-forever'
+#     lm_c.command 'run-forever'
+#   end
+# end
 
 # turn_left()
 
@@ -39,14 +39,14 @@ lm_c.command 'stop'
 def turn_left(degree)
   magic_value = degree * 1
 
-  lm_b.duty_cycle_sp -50
-  lm_b.time_sp 2000
+  lm_b.duty_cycle_sp 70
+  lm_b.time_sp magic_value
 
-  lm_c.duty_cycle_sp -50
-  lm_c.time_sp 2000
+  lm_c.duty_cycle_sp -70
+  lm_c.time_sp magic_value
 
   lm_b.command 'run-timed'
   lm_c.command 'run-timed'
-  sleep 4
+  sleep magic_value
 end
 
