caaee98f9d295c587a70087879b6b21b7854854c
Nandor Biro <nandor.biro@digitalnatives.hu>
2016-11-11 11:44:06 +0100

    Moving between arrays

--- lego_array.rb
+++ lego_array.rb
@@ -2,31 +2,69 @@ require './motor'
 require './color_sensor'
 
 class LegoArray
-  attr_reader :motor, :color_sensor, :array
+  attr_reader :motor, :color_sensor, :array, :current_position
 
-  def initialize(array)
+  def initialize(array, starting_position)
     @motor = Motor.new
     @color_sensor = ColorSensor.new
     @array = array
+    @current_position = starting_position
   end
 
-  def next
-    while @color_sensor.current_color != 'White'
-      @motor.move_forward
+  def next_position
+    while color_sensor.current_color != 'White'
+      motor.move_forward
     end
 
-    while @color_sensor.current_color != 'Black'
-      @motor.move_forward
+    while color_sensor.current_color != 'Black'
+      motor.move_forward
     end
   end
 
-  def previous
-    while @color_sensor.current_color != 'White'
-      @motor.move_backward
+  def previous_position
+    while color_sensor.current_color != 'White'
+      motor.move_backward
     end
 
-    while @color_sensor.current_color != 'Black'
-      @motor.move_backward
+    while color_sensor.current_color != 'Black'
+      motor.move_backward
     end
   end
+
+  def go_to_tmp
+  end
+
+  def go_to(target_position)
+    return go_to_tmp if target_position == -1
+
+    unless array[target_position].nil?
+      # move forward
+      if current_position < target_position
+        steps = if current_position == -1
+                  target_position + 1
+                else
+                  target_position - current_position
+                end
+
+        steps.times do
+          next_position
+        end
+        current_position = target_position
+      # stay there
+      elsif current_position == target_position
+        return false
+      # move backward
+      else
+        steps = current_position - target_position
+        steps.times do
+          previous_position
+        end
+        current_position = target_position
+      end
+
+    else
+      return false
+    end
+  end
+
 end
--- strategy.rb
+++ strategy.rb
@@ -1,6 +1,13 @@
 require './lego_array'
 
-lego_array = LegoArray.new []
+lego_array = LegoArray.new([6,7,4,47], -1)
 
-lego_array.next
-lego_array.previous
+lego_array.next_position
+lego_array.previous_position
+
+lego_array.go_to(-1)
+lego_array.go_to(1)
+lego_array.go_to(2)
+lego_array.go_to(0)
+lego_array.go_to(3)
+lego_array.go_to(4)
