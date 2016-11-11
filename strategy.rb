acb787898549b78e0efb7571d58ef34b7550476c
Nandor Biro <nandor.biro@digitalnatives.hu>
2016-11-11 10:42:23 +0100

    Cleanup strategies

--- strategy.rb
+++ strategy.rb
@@ -1,17 +1,6 @@
-require './motor'
-require './color_sensor'
+require './lego_array'
 
-motor = Motor.new
-color_sensor = ColorSensor.new
+lego_array = LegoArray.new
 
-current_color = color_sensor.current_color
-
-while current_color != 'Red'
-  motor.move_forward
-  current_color = color_sensor.current_color
-end
-
-while current_color != 'Yellow'
-  motor.rotate_left
-  current_color = color_sensor.current_color
-end
+LegoArray.next
+LegoArray.previous
